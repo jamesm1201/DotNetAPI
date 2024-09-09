2c0d4dd9468f400cc5b913e01c2dc1e773b494e3
James <jhmorley@icloud.com>
2024-09-09 11:17:06 +0100

    check branch works

--- obj/Debug/net8.0/DotNetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b37be87e1bd3a4843d52b7633fe6ad7e08b23fa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
