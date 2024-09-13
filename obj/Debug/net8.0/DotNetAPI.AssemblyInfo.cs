60a23b638c8dc65f6b00693ec0a3e0675af212a8
James <jhmorley@icloud.com>
2024-09-13 17:27:43 +0100

    Uknown Error

--- obj/Debug/net8.0/DotNetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46805f57521e93326c0b124b4c17a43019927f1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ff319782a9b3b8ed3070d9bd2148b5eba54c62")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
